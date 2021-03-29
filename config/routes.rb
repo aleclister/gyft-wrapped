6e323cd7b7f25f9eca54f5dc0e491cd6f74a7137
Alec Lister <alec.lister@gmail.com>
2021-03-28 22:39:46 -0400

    Added about, contact + controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root            to: 'pages#home'
+  get 'about',    to: 'pages#about' ,  as: :about
+  get 'contact',  to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
