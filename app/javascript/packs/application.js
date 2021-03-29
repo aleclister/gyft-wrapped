f4e822c26905b8f59538a4f3be0267868597358e
Alec Lister <alec.lister@gmail.com>
2021-03-29 11:56:47 -0400

    Added navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("channels")
 
 import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
