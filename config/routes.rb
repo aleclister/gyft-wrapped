a4c70a6f231cfdaa5e8d0a3db6133a62a40e8eab
Alec Lister <alec.lister@gmail.com>
2021-03-29 08:33:40 -0400

    Added relations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root            to: 'pages#home'
-  get 'about',    to: 'pages#about' ,  as: :about
-  get 'contact',  to: 'pages#contact', as: :contact
+  get 'about',    to: 'pages#about' ,     as: :about
+  get 'contact',  to: 'pages#contact',    as: :contact
+  get 'relations' to: 'relations#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
