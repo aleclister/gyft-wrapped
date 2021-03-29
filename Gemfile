7b72950408573323d5d60a78ebad5e09094d8ad2
Alec Lister <alec.lister@gmail.com>
2021-03-29 15:04:30 -0400

    Added Admin dashboard

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'devise'
 gem 'pundit'
+gem 'rails_admin', '~> 2.0'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_160603) do
+ActiveRecord::Schema.define(version: 2021_03_29_184707) do
 
   create_table "hobbies", force: :cascade do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_03_29_160603) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
