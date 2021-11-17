510a43b0b022d66509fbb558144ed5c57340a585
Alec Lister <alec.lister@gmail.com>
2021-11-17 14:41:58 -0700

    number of relations bug on dashboard. redirect devise to dashboard after login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,15 @@ class ApplicationController < ActionController::Base
   include Pundit
 
 
+  protected
 
+  def after_sign_in_path_for(resource)
+    '/dashboard'
+  end
+
+  def after_sign_out_path_for(scope)
+    '/'
+  end
 
 
   private
